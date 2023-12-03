3d42ccaaa22f8bd02aa1cf413f828a454b411c8b
= <erik.aho24@gmail.com>
2023-12-03 21:07:28 +0000

    redo days

---
+++ scripts/day20.py
@@ -0,0 +1,9 @@
+DAY_NUM = "20"
+
+def main(inputs):
+    pass
+
+if __name__ == "__main__":
+    with open(f"inputs/{DAY_NUM}/input.txt", "r") as file:
+        inputs = file.read().split()
+    main(inputs)
---
+++ scripts/day22.py
@@ -0,0 +1,9 @@
+DAY_NUM = "22"
+
+def main(inputs):
+    pass
+
+if __name__ == "__main__":
+    with open(f"inputs/{DAY_NUM}/input.txt", "r") as file:
+        inputs = file.read().split()
+    main(inputs)
