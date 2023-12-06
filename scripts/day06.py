ba1cef6ce029e99b04a69563de124eaba2c16f14
= <erik.aho24@gmail.com>
2023-12-06 12:12:31 +0000

    Day 6

--- scripts/day06.py
+++ scripts/day06.py
@@ -1,9 +1,39 @@
+import numpy as np
+
 DAY_NUM = "06"
 
+
 def main(inputs):
-    pass
+    times = list(map(int, inputs[0].split()[1:]))
+    distances = list(map(int, inputs[1].split()[1:]))
+
+    def find_intersection(d, t):
+        # just returns lower intersection
+        lower = 0
+        upper = (t // 2) + 1
+        while (upper - lower) > 1:
+            mid = (upper + lower + 1) // 2
+            if mid * (t - mid) <= d:
+                lower = mid
+            else:
+                upper = mid
+        return upper
+
+    def get_wins(d, t):
+        low = find_intersection(d, t)
+        high = t - low
+        return high + 1 - low
+
+    wins = [get_wins(distances[i], times[i]) for i in range(len(times))]
+    print(f"Part 1: {np.prod(wins)}")
+
+    real_time = int("".join(map(str, times)))
+    real_dist = int("".join(map(str, distances)))
+
+    print(f"Part 2: {get_wins(real_dist, real_time)}")
+
 
 if __name__ == "__main__":
     with open(f"inputs/{DAY_NUM}/input.txt", "r") as file:
-        inputs = file.read().split()
+        inputs = [x.rstrip() for x in file.readlines()]
     main(inputs)
