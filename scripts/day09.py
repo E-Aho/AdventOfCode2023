729f37606fc8a03d29e6905181eacc4b8e81ee08
= <erik.aho24@gmail.com>
2023-12-09 15:50:55 +0000

    Day 9

--- scripts/day09.py
+++ scripts/day09.py
@@ -1,9 +1,23 @@
 DAY_NUM = "09"
 
+def get_next(hist):
+    """Recursively check until vals are same, return next"""
+    if hist[0] == hist[-1]:
+        return hist[0]
+    deltas = [hist[i+1] - hist[i] for i in range(len(hist)-1)]
+    return hist[-1] + get_next(deltas)
+
 def main(inputs):
-    pass
+    # p1
+    p1_outs = [get_next(h) for h in inputs]
+    print(sum(p1_outs))
+
+    # p2
+    reversed_inputs = [list(reversed(h)) for h in inputs]
+    p2_outs = [get_next(h) for h in reversed_inputs]
+    print(sum(p2_outs))
 
 if __name__ == "__main__":
     with open(f"inputs/{DAY_NUM}/input.txt", "r") as file:
-        inputs = [x.rstrip() for x in file.readlines()]
+        inputs = [list(map(int, x.rstrip().split())) for x in file.readlines()]
     main(inputs)
