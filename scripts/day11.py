b271f21eebb8a6159fe5a861dc53d030ddf69ca8
= <erik.aho24@gmail.com>
2023-12-11 17:19:14 +0000

    Clean up day 11

--- scripts/day11.py
+++ scripts/day11.py
@@ -4,28 +4,26 @@ from itertools import combinations
 DAY_NUM = "11"
 
 
-def get_galaxies(inputs, expansion_factor: int = 1):
+def get_raw_galaxies(inputs):
     galaxies = []
-    xs, ys, = [], []
+    xs, ys, = set(), set()
     for y, line in enumerate(inputs):
         for x, char in enumerate(line):
             if char == "#":
                 galaxies.append((x, y))
-                xs.append(x)
-                ys.append(y)
-
-    # find empty xs and ys
-    xs = set(xs)
-    ys = set(ys)
-
+                xs.add(x)
+                ys.add(y)
     empty_x = sorted(list(set(range(len(inputs[0]))) - xs))
     empty_y = sorted(list(set(range(len(inputs))) - ys))
+    return galaxies, empty_x, empty_y
 
+
+def expand_galaxies(galaxies, empty_xs, empty_ys, expansion_factor: int = 1):
     # expand based on bisected index into sorted empty space arrays
     fixed_galaxies = {
         (
-            x + bisect(empty_x, x) * (expansion_factor - 1),
-            y + bisect(empty_y, y) * (expansion_factor - 1)
+            x + bisect(empty_xs, x) * (expansion_factor - 1),
+            y + bisect(empty_ys, y) * (expansion_factor - 1)
         ) for x, y in galaxies
     }
     return fixed_galaxies
@@ -36,10 +34,11 @@ def manhattan_dist(i, j):
 
 
 def main(inputs):
-    for part, expansion_factor in [("P1", 2), ("P2", 1000000)]:
-        galaxies = get_galaxies(inputs, expansion_factor)
+    raw_galaxies, empty_x, empty_y = get_raw_galaxies(inputs)
+    for part, factor in [("P1", 2), ("P2", 1000000)]:
+        expanded_galaxies = expand_galaxies(raw_galaxies, empty_x, empty_y, factor)
         total_dist = 0
-        for i, j in combinations(galaxies, 2):
+        for i, j in combinations(expanded_galaxies, 2):
             total_dist += manhattan_dist(i, j)
 
         print(f"{part}: {total_dist}")
